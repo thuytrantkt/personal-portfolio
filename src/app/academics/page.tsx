f679c21f727b007e4b1435c4c2d562ca32bcf252
thuytrantkt <christiesunnie@gmail.com>
2025-05-25 20:26:44 -0400

    Create Hero component

--- src/app/academics/page.tsx
+++ src/app/academics/page.tsx
@@ -3,19 +3,27 @@
 import { Card, CardContent, List, ListItem, Typography } from "@mui/joy";
 import "../ui/academics.css";
 import useBreakpoint from "../hooks/useBreakpoints";
+import Hero from "@/components/Hero/Hero";
 
 export default function Academics() {
   const lgScreen = useBreakpoint("lg");
 
   return (
-    <div>
-      <h2>Academics</h2>
-      <h3>Educational Background</h3>
-      <p>
-        My academic journey, courses, and educational achievements that have
-        shaped my technical expertise.
-      </p>
-      <h4>Education</h4>
+    <>
+      <Hero
+        heading="Academics"
+        subhead="Educational Background"
+        description="My academic journey, courses, and educational achievements that have
+        shaped my technical expertise."
+      />
+
+      <Typography
+        level="h2"
+        component="h3"
+        sx={{ textAlign: "center", paddingBottom: "48px" }}
+      >
+        Education
+      </Typography>
       <div
         className={`${
           lgScreen ? "education-container-lg" : "education-container"
@@ -67,58 +75,62 @@ export default function Academics() {
         </Card>
       </div>
 
-      <div>
-        <h4>Certifications</h4>
+      <Typography
+        level="h2"
+        component="h3"
+        sx={{ textAlign: "center", paddingBottom: "48px" }}
+      >
+        Certifications
+      </Typography>
 
-        <div
-          className={`${
-            lgScreen ? "certificates-container-lg" : "certificates-container"
-          }`}
-        >
-          <Card variant="solid">
-            <CardContent>
-              <Typography textColor="inherit">
-                AWS Certified Cloud Practitioner
-              </Typography>
-              <Typography textColor="inherit">
-                Amazon Web Services Training and Certification
-              </Typography>
-              <Typography textColor="inherit">2025</Typography>
-              <Typography textColor="inherit">
-                A fundamental understanding of IT services and their uses in the
-                AWS Cloud. This certificate demonstrated cloud fluency and
-                foundational AWS knowledge. The certified practitioner is able
-                to identify essential AWS services necessary to set up
-                AWS-focused projects.
-              </Typography>
-            </CardContent>
-          </Card>
+      <div
+        className={`${
+          lgScreen ? "certificates-container-lg" : "certificates-container"
+        }`}
+      >
+        <Card variant="solid">
+          <CardContent>
+            <Typography textColor="inherit">
+              AWS Certified Cloud Practitioner
+            </Typography>
+            <Typography textColor="inherit">
+              Amazon Web Services Training and Certification
+            </Typography>
+            <Typography textColor="inherit">2025</Typography>
+            <Typography textColor="inherit">
+              A fundamental understanding of IT services and their uses in the
+              AWS Cloud. This certificate demonstrated cloud fluency and
+              foundational AWS knowledge. The certified practitioner is able to
+              identify essential AWS services necessary to set up AWS-focused
+              projects.
+            </Typography>
+          </CardContent>
+        </Card>
 
-          <Card variant="solid">
-            <CardContent>
-              <Typography textColor="inherit">
-                JavaScript Unit Testing - The Practical Guide
-              </Typography>
-              <Typography textColor="inherit">Udemy</Typography>
-              <Typography textColor="inherit">2024</Typography>
-              <List marker="disc">
-                <ListItem sx={{ color: "white" }}>
-                  Write and structure unit & integration tests
-                </ListItem>
-                <ListItem sx={{ color: "white" }}>
-                  Remove side effects from tests via mocking and spies
-                </ListItem>
-                <ListItem sx={{ color: "white" }}>
-                  Work with popular JavaScript test runners and libraries
-                </ListItem>
-                <ListItem sx={{ color: "white" }}>
-                  Write good tests and focus on testing core business logic
-                </ListItem>
-              </List>
-            </CardContent>
-          </Card>
-        </div>
+        <Card variant="solid">
+          <CardContent>
+            <Typography textColor="inherit">
+              JavaScript Unit Testing - The Practical Guide
+            </Typography>
+            <Typography textColor="inherit">Udemy</Typography>
+            <Typography textColor="inherit">2024</Typography>
+            <List marker="disc">
+              <ListItem sx={{ color: "white" }}>
+                Write and structure unit & integration tests
+              </ListItem>
+              <ListItem sx={{ color: "white" }}>
+                Remove side effects from tests via mocking and spies
+              </ListItem>
+              <ListItem sx={{ color: "white" }}>
+                Work with popular JavaScript test runners and libraries
+              </ListItem>
+              <ListItem sx={{ color: "white" }}>
+                Write good tests and focus on testing core business logic
+              </ListItem>
+            </List>
+          </CardContent>
+        </Card>
       </div>
-    </div>
+    </>
   );
 }
