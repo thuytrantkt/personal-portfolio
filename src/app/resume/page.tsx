650c27766e076cfdb750d86ba9ece2509b8f0c75
thuytrantkt <christiesunnie@gmail.com>
2025-07-27 12:41:44 -0400

    Update the projects in Resume section

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -170,16 +170,17 @@ export default function Resume() {
                   textColor="inherit"
                   startDecorator={<CodeOutlined aria-labelledby="Code icon" />}
                 >
-                  Soft Skills
+                  Technical Stack
                 </Typography>
                 <ul>
-                  <li>HTML/CSS</li>
-                  <li>JavaScript</li>
-                  <li>Ruby</li>
-                  <li>SQL</li>
-                  <li>React Native</li>
-                  <li>Tailwind CSS</li>
+                  <li>HTML</li>
+                  <li>CSS</li>
+                  <li>TailwindCSS</li>
                   <li>SASS</li>
+                  <li>JavaScript</li>
+                  <li>TypeScript</li>
+                  <li>Ruby</li>
+                  <li>React Native</li>
                 </ul>
               </CardContent>
             </Card>
@@ -215,13 +216,15 @@ export default function Resume() {
                   Tools & Technologies
                 </Typography>
                 <ul>
-                  <li>Git/GitHub</li>
+                  <li>Git</li>
+                  <li>GitHub</li>
                   <li>Figma</li>
-                  <li>VS Code</li>
+                  <li>Visual Studio Code</li>
                   <li>Postman</li>
                   <li>Confluence</li>
                   <li>Jira</li>
                   <li>BitBucket</li>
+                  <li>Azure DevOps</li>
                   <li>Azure Data Studio</li>
                   <li>Docker</li>
                 </ul>
@@ -371,10 +374,10 @@ export default function Resume() {
                     maintainable codebases.
                   </li>
                   <li>
-                    Drove innovation in front-end development through advanced
-                    accessibility implementations and responsive design
-                    patterns, ensuring a consistent user experience across
-                    multiple browsers and devices.
+                    Strategized the powerful data fetching and caching tool to
+                    simplify loading data in the web application and the data
+                    flow between the back-end and front-end systems with the use
+                    of Redux Toolkit Query.
                   </li>
                   <li>
                     Guided junior developers through complex technical
@@ -484,14 +487,55 @@ export default function Resume() {
                   <Typography textColor="inherit" fontWeight="bold">
                     Key Responsibilities:
                   </Typography>
-                  <li>Key 1</li>
-                  <li>Key 2</li>
-                  <li>Key 3</li>
+                  <li>
+                    Implement a UI wrapper uses the existing business logic
+                    embedded within the platform but displays the information in
+                    a new way to improve user adoption and reduce friction with
+                    the tool upon go-live, improve productivity with a more
+                    intuitive, faster experience and enable long-term product
+                    roadmap through scalable systems.
+                  </li>
+                  <li>
+                    Lead multiple features within this project alongside with a
+                    team of designers, back-end developers, and product managers
+                    to provide insights from a technical perspective, flagging
+                    potential delays, raising concerns and issues, and
+                    estimating timelines for the work to ensure the successful
+                    completion of the project.
+                  </li>
+                  <li>
+                    Possess strong leadership skills, including the ability to
+                    communicate effectively, delegate tasks, and provide
+                    guidance to team members, and valuable technical insight and
+                    leadership to ensure the project is completed on time and to
+                    the highest possible standard.
+                  </li>
+                  <li>
+                    Utilized the underlying back-end business logic and
+                    architecture to accelerate the front-end development.
+                  </li>
+                  <li>
+                    Strategized the powerful data fetching and caching tool to
+                    simplify loading data in the web application and the data
+                    flow between the back-end and front-end systems with the use
+                    of Redux Toolkit Query.
+                  </li>
+                  <li>
+                    Collaborated with the testing team to build the testing plan
+                    in advance of Beta User testing for 21 new redesigned.
+                  </li>
                 </ul>
 
-                <ul>
-                  <li>React</li>
+                <ul className="list-disc p-6">
+                  <Typography textColor="inherit" fontWeight="bold">
+                    Technologies:
+                  </Typography>
+                  <li>HTML</li>
                   <li>Tailwind CSS</li>
+                  <li>React</li>
+                  <li>TypeScript</li>
+                  <li>RESTful API integration</li>
+                  <li>Redux Toolkit Query</li>
                 </ul>
               </CardContent>
             </Card>
@@ -506,14 +550,87 @@ export default function Resume() {
                   <Typography textColor="inherit" fontWeight="bold">
                     Key Responsibilities:
                   </Typography>
-                  <li>Key 1</li>
-                  <li>Key 2</li>
-                  <li>Key 3</li>
+                  <li>
+                    Implemented a new single, modernized service available in
+                    both official languages (English and French) which features
+                    interfaces that empower users to manage renewal reminders on
+                    SNB.ca and choose to receive, or opt out of, either email or
+                    printed vehicle registration renewal notifications using
+                    ReactJS, TypeScript, NodeJS, MySQL, and RESTful API.
+                  </li>
+                  <li>
+                    Implemented system to authenticate users, enabling access to
+                    pre-filled information and details for VRR and Reminders
+                    (vehicle VIN number, license plate(s), driver license,
+                    insurance info).
+                  </li>
+                  <li>
+                    Developed a solution that align seamlessly with the GNB
+                    Design System, ensuring a consistent and user-friendly
+                    interface. Furthermore, it adheres rigorously to SNB's
+                    standards, encompassing language, security, privacy, and
+                    accessibility, guaranteeing a robust and compliant product
+                    that meets all regulatory requirements.
+                  </li>
+                  <li>
+                    Configured the interaction with existing backend APIs are
+                    available and can be leveraged to perform the various
+                    outlined function such as user feedback and improvement,
+                    profile management and reminder execution.
+                  </li>
+                  <li>
+                    Configured and developed functionality and user interfaces
+                    to build the PR portal and documented Technical Design
+                    Specifications and Technical Interface Documents.
+                  </li>
+                  <li>
+                    Executed functional unit testing, technical unit testing,
+                    security unit testing, integration testing, regression
+                    testing, operational readiness testing, and performance
+                    testing.
+                  </li>
+                  <li>
+                    Developed the deployment strategy, deployment plan, and
+                    go-live rehearsals.
+                  </li>
+                  <li>
+                    Tracked the deployment activities by task and start and end
+                    times, and assigned the work to team members.
+                  </li>
+                  <li>
+                    Developed a business and technical roadmap to showcase the
+                    current state, evolution plan, and desired future state for
+                    the project, including a mapping of delivered capabilities
+                    across all phases.
+                  </li>
+                  <li>
+                    Ongoing maintenance and support of the PR portal, provided
+                    following each release to address production issues.
+                  </li>
+                  <li>
+                    Worked closely and collaboratively with the external team to
+                    identify and agree upon key areas of knowledge transfer as
+                    the client’s team sets up internal capabilities to support
+                    and grow the PR Digital product.
+                  </li>
+                  <li>
+                    Managed day-to-day execution of project plan, reported
+                    regular status against project plan, escalated issues /
+                    risks to program leadership with preliminary
+                    recommendations.
+                  </li>
                 </ul>
-
-                <ul>
+                <ul className="list-disc p-6">
+                  <Typography textColor="inherit" fontWeight="bold">
+                    Technologies:
+                  </Typography>
+                  <li>HTML</li>
                   <li>React</li>
+                  <li>TypeScript</li>
                   <li>Tailwind CSS</li>
+                  <li>RESTful API integration</li>
+                  <li>PostgreSQL</li>
+                  <li>NodeJS, ExpressJS</li>
                 </ul>
               </CardContent>
             </Card>
@@ -528,14 +645,81 @@ export default function Resume() {
                   <Typography textColor="inherit" fontWeight="bold">
                     Key Responsibilities:
                   </Typography>
-                  <li>Key 1</li>
-                  <li>Key 2</li>
-                  <li>Key 3</li>
+                  <li>
+                    Builds reusable and scalable UI components to develop large
+                    front- end web applications that scale and perform well on
+                    all devices using different technologies and frameworks like
+                    HTML, CSS, TypeScript, JavaScript, React, Ruby on Rails, and
+                    GraphQL.
+                  </li>
+                  <li>
+                    Supports and mentors junior and intermediate team members by
+                    sharing knowledge, demoing, documenting, and providing
+                    feedback to ensure that code standards and deadlines are
+                    consistently met. GraphQL.
+                  </li>
+                  <li>
+                    A major contributor to creating the interactive experience
+                    for users with animation using SVG turbulence filter,
+                    Parallax.js and GSAP. Executed and maintained 200+unit and
+                    integration tests as a part of the development workflow and
+                    improved website performance, optimization, and
+                    responsiveness across devices and browsers using Jest,
+                    Mocha, and ActiveRecord::Fixtures. GraphQL.
+                  </li>
+                  <li>
+                    Implement site mapping with several locale-aware domains
+                    that offer a native experience to millions of merchants
+                    globally alongside SEO specialists and the translation team
+                    to provide localized content via the Argo system. Migrate
+                    several legacy existing marketing pages to Remixing
+                    Hydrogen, Shopify's React-based headless commerce stack that
+                    features a better performance with advancements like
+                    optimistic UI, nested routes, and progressive enhancement.
+                    GraphQL.
+                  </li>
+                  <li>
+                    Redesign and restructure the existing marketing pages' web
+                    experience by using the Flipper UI feature flag to manage
+                    complex version controls in a large scope of project and
+                    feature experiments. Develop an A/B test for Shopify
+                    Capital's UI experience to increase the conversion rate with
+                    a lift of 11% representing $1.7M+ estimated incremental
+                    funding opportunity annually. GraphQL.
+                  </li>
+                  <li>
+                    Collaborate with Agile cross-functional teams of engineers,
+                    designers, and product managers by coding, pair programming
+                    with code reviews, and white-boarding solutions to plan and
+                    estimate development work. Work with the stakeholders on the
+                    scope project to flag potential delays and propose technical
+                    solutions for user experience, ensuring the alignment of web
+                    design, maximum efficiency, and maintaining brand
+                    consistency across web pages. GraphQL.
+                  </li>
+                  <li>
+                    Use Ruby on Rails to add and modify controllers, models,
+                    views, and integrate front-end code into Rails web
+                    applications. GraphQL.
+                  </li>
+                  <li>
+                    Maintain and build web apps that meet WCAG 2.1 AA compliant
+                    by using automated and manual testing tools and following
+                    best practices in the design system.
+                  </li>
                 </ul>
 
-                <ul>
+                <ul className="list-disc p-6">
+                  <Typography textColor="inherit" fontWeight="bold">
+                    Technologies:
+                  </Typography>
+                  <li>HTML</li>
                   <li>React</li>
                   <li>Tailwind CSS</li>
+                  <li>Ruby</li>
+                  <li>Ruby on Rails</li>
+                  <li>GraphQL</li>
+                  <li>TypeScript</li>
                 </ul>
               </CardContent>
             </Card>
